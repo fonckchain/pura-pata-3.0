7a9631dc865249a724e5288a3b51d8293b6a3a32
fonckchain <afonck@protonmail.com>
2025-11-21 21:16:01 -0600

    favicon fix: moved to public

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -39,17 +39,16 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  // icons: {
-  //   icon: [
-  //     { url: '/logo.svg', type: 'image/svg+xml' },
-  //     { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-  //     { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-  //   ],
-  //   apple: [
-  //     { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
-  //   ],
-  //   shortcut: '/favicon.ico',
-  // },
+  icons: {
+    icon: [
+      { url: '/logo.svg', type: 'image/svg+xml' },
+      { url: '/icon.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+  },
   manifest: '/manifest.json',
   openGraph: {
     type: 'website',
